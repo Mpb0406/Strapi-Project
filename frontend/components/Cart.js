4ccc78e4fa4bf6263112f9b1af1a2ff05789befd
Mike Bolloskis <mpb0406@gmail.com>
2022-07-03 20:43:15 -0400

    Added animation to toggle cart and cart items

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -15,17 +15,35 @@ const Cart = () => {
   const { cartItems, setShowCart, onAdd, onRemove, totalPrice } =
     useStateContext();
   return (
-    <CartWrapper onClick={() => setShowCart(false)}>
-      <CartStyle onClick={(e) => e.stopPropagation()}>
+    <CartWrapper
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      onClick={() => setShowCart(false)}>
+      <CartStyle
+        initial={{ x: "50%" }}
+        animate={{ x: "0%" }}
+        exit={{ x: "50%" }}
+        onClick={(e) => e.stopPropagation()}>
         {cartItems.length < 1 && (
-          <EmptyStyle>
+          <EmptyStyle
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+              transition: { delay: 0.1, duration: 0.2 },
+            }}>
             <h1>No items in cart...</h1>
             <FaShoppingCart />
           </EmptyStyle>
         )}
         {cartItems.length >= 1 &&
           cartItems.map((item) => (
-            <Card key={item.slug}>
+            <Card
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ delay: 0.3 }}
+              key={item.slug}>
               <img
                 src={item.image.data.attributes.formats.thumbnail.url}
                 alt={item.title}
--- frontend/styles/CartStyles.js
+++ frontend/styles/CartStyles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+const { motion } = require("framer-motion");
 
-export const CartWrapper = styled.div`
+export const CartWrapper = styled(motion.div)`
   position: fixed;
   right: 0;
   top: 0;
@@ -13,15 +14,16 @@ export const CartWrapper = styled.div`
   /* display: none; */
 `;
 
-export const CartStyle = styled.div`
+export const CartStyle = styled(motion.div)`
   width: 35%;
   background: #f1f1f1;
   padding: 2rem 1.5rem;
   overflow-y: scroll;
   position: relative;
+  overflow-x: hidden;
 `;
 
-export const Card = styled.div`
+export const Card = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -36,7 +38,7 @@ export const Card = styled.div`
   }
 `;
 
-export const CardInfo = styled.div`
+export const CardInfo = styled(motion.div)`
   width: 50%;
   div {
     display: flex;
@@ -44,10 +46,9 @@ export const CardInfo = styled.div`
   }
 `;
 
-export const EmptyStyle = styled.div`
+export const EmptyStyle = styled(motion.div)`
   position: absolute;
   top: 0;
-  left: 50%;
   transform: translate(-50%, 0);
   display: flex;
   flex-direction: column;
@@ -58,7 +59,7 @@ export const EmptyStyle = styled.div`
 
   h1 {
     font-size: 2rem;
-    padding: 2rem;
+    padding: 2rem 0;
   }
 
   svg {
@@ -67,7 +68,7 @@ export const EmptyStyle = styled.div`
   }
 `;
 
-export const Checkout = styled.div`
+export const Checkout = styled(motion.div)`
   button {
     background: var(--primary);
     padding: 1rem 2rem;
